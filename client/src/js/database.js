5c0471a2d4eb1dac2db960b7e54d2d20dd4d48cf
DPLong <dplong742@gmail.com>
2022-08-07 17:32:25 -0400

    mostly functional

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,35 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Post to database');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const jateDb = await openDB('jate', 1);
+
+  const transact = jateDb.transaction('jate', 'readwrite');
+
+  const store = transact.objectStore('jate');
+
+  const request = store.put({ id: 1, value: content });
+
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
+
+export const getDb = async () => {
+  console.log('Get from database');
+  
+  const jateDb = await openDB('jate', 1);
+  
+  const transact = jateDb.transaction('jate', 'readonly');
+  
+  const store = transact.objectStore('jate');
+  
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,4 +1,3 @@
-// Import methods to save and get data from the indexedDB database in './database.js'
 import { getDb, putDb } from './database';
 import { header } from './header';
 
@@ -6,9 +5,8 @@ export default class {
   constructor() {
     const localData = localStorage.getItem('content');
 
-    // check if CodeMirror is loaded
     if (typeof CodeMirror === 'undefined') {
-      throw new Error('CodeMirror is not loaded');
+      throw new Error('CodeMirror is not loaded.');
     }
 
     this.editor = CodeMirror(document.querySelector('#main'), {
@@ -22,10 +20,8 @@ export default class {
       tabSize: 2,
     });
 
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
+      console.info('Loaded from IndexedDB.');
       this.editor.setValue(data || localData || header);
     });
 
@@ -33,9 +29,8 @@ export default class {
       localStorage.setItem('content', this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself is loses focus
     this.editor.on('blur', () => {
-      console.log('The editor has lost focus');
+      console.log('The editor is no longer in focus.');
       putDb(localStorage.getItem('content'));
     });
   }
